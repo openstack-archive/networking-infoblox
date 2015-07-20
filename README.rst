bf0e3d3bb6317f51a09ea6b6964a479579f6ed4d
John Belamaric <jbelamaric@infoblox.com>
2015-07-20 17:30:08 -0400

    Fix newlines and testenvs Add devstack plugin shell
    Change-Id: I25b613df8469a5f1536798d3a280045a51d00e03

--- README.rst
+++ README.rst
@@ -4,14 +4,21 @@ networking-infoblox
 
 Contains Neutron drivers for integration with Infoblox grids for IPAM and DNS.
 
-Please feel here a long description which must be at least 3 lines wrapped on
-80 cols, so that distribution package maintainers can use it in their packages.
-Note that this is a hard requirement.
-
+OpenStack Liberty 
 * Free software: Apache license
 * Documentation: http://docs.openstack.org/developer/networking-infoblox
 * Source: http://git.openstack.org/cgit/openstack/networking-infoblox
 * Bugs: http://bugs.launchpad.net/networking-infoblox
+
+Enabling In DevStack
+--------------------
+
+To enable use of Infoblox in DevStack, add this repository as a plugin:
+
+     enable_plugin networking-infoblox https://git.openstack.org/openstack/networking-infoblox.git
+     enable_service networking-infoblox
+
+You must also configure the specific Infoblox functionality you wish to use.
 
 Features
 --------
---
+++ devstack/plugin.sh
@@ -0,0 +1,51 @@
+# plugin.sh - DevStack plugin.sh dispatch script networking_infoblox
+
+function install_networking_infoblox {
+    echo 
+}
+
+function init_networking_infoblox {
+    echo
+}
+
+function configure_networking_infoblox {
+    echo
+}
+
+# check for service enabled
+if is_service_enabled networking-infoblox; then
+
+    if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+        # Set up system services
+        echo_summary "Configuring system services for Infoblox Networking"
+        #install_package cowsay
+
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # Perform installation of service source
+        echo_summary "Installing Infoblox Networking"
+        install_networking_infoblox
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # Configure after the other layer 1 and 2 services have been configured
+        echo_summary "Configuring Infoblox Networking"
+        configure_networking_infoblox
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        # Initialize and start the networking-infoblox service
+        echo_summary "Initializing Infoblox Networking"
+        init_networking_infoblox
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # Shut down networking_infoblox services
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # Remove state and transient data
+        # Remember clean.sh first calls unstack.sh
+        # no-op
+        :
+    fi
+fi
