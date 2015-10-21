fcc683f6610ea3ace072b0af0db77f9c39403860
Pavel Bondar <pbondar@infoblox.com>
2015-10-21 19:08:00 +0300

    [DO NOT MERGE] Implement basic ipam driver
    Contains ipam driver with minimal functionality:
    - allocate subnet
    - get subnet
    - delete subnet
    Uses single network view mode, 'default' network view is used for now.
    Newly created network on NIOS side is tagged with 'Subnet ID' EA.
    
    Change-Id: Ifb00ab24032cce68b883480a51afe576c08828fc

--- networking_infoblox/tests/unit/ipam/test_driver.py
+++ networking_infoblox/tests/unit/ipam/test_driver.py
@@ -14,6 +14,9 @@
 #    under the License.
 
 import mock
+import netaddr
+
+from neutron.ipam import requests
 
 from networking_infoblox.neutron.ipam import driver as drv
 from networking_infoblox.tests import base
@@ -25,12 +28,105 @@ class TestDriver(base.TestCase):
         self.assertIsInstance(drv.InfobloxPool(mock.Mock(), mock.Mock()),
                               drv.InfobloxPool)
 
-    def test_get_subnet(self):
-        driver = drv.InfobloxPool(mock.Mock(), mock.Mock())
+    def _mock_driver(self, subnet_pool=None, cidr='192.168.10.0/24',
+                     gateway='192.168.10.1'):
+        driver = drv.InfobloxPool(subnet_pool, mock.Mock())
+        driver._get_connector = mock.Mock(return_value='connector')
+        driver._fetch_subnet = mock.Mock(return_value={
+            'id': 'subnet-id',
+            'cidr': cidr,
+            'tenant_id': 'tenant-id',
+            'gateway_ip': '192.168.10.1'})
+        return driver
+
+    @mock.patch('infoblox_client.objects.Network')
+    def test_get_subnet(self, net):
+        driver = self._mock_driver()
+
         ipam_subnet = driver.get_subnet(mock.Mock())
+
+        self.assertTrue(driver._get_connector.called)
+        net.search.assert_called_with('connector',
+                                      network_view='default',
+                                      network='192.168.10.0/24')
+
         self.assertIsInstance(ipam_subnet, drv.InfobloxSubnet)
 
-    def test_allocate_subnet(self):
-        driver = drv.InfobloxPool(mock.Mock(), mock.Mock())
-        ipam_subnet = driver.allocate_subnet(mock.Mock())
+    @mock.patch('infoblox_client.objects.Network')
+    def test_allocate_subnet(self, net):
+        driver = self._mock_driver(None)
+        pools = [netaddr.ip.IPRange('192.168.10.3', '192.168.10.25')]
+
+        request = requests.SpecificSubnetRequest(
+            'tenant_id', 'subnet_id', '192.168.10.0/24',
+            '192.168.10.1', pools)
+        ipam_subnet = driver.allocate_subnet(request)
         self.assertIsInstance(ipam_subnet, drv.InfobloxSubnet)
+
+    @mock.patch('infoblox_client.objects.Network')
+    def test_remove_subnet(self, net):
+        nios_net = mock.Mock()
+        net.search = mock.Mock(return_value=nios_net)
+        driver = self._mock_driver()
+
+        driver.remove_subnet('subnet-id')
+
+        self.assertTrue(driver._get_connector.called)
+        net.search.assert_called_with('connector',
+                                      network_view='default',
+                                      network='192.168.10.0/24')
+
+        nios_net.delete.assert_called_with('connector')
+
+    @mock.patch('infoblox_client.connector.Connector')
+    def test_get_connector(self, conn_mock):
+        expected_opts = {'host': '',
+                         'username': '',
+                         'password': '',
+                         'wapi_version': '',
+                         'ssl_verify': False,
+                         'http_pool_connections': 100,
+                         'http_pool_maxsize': 100,
+                         'http_request_timeout': 120}
+        conn_mock._parse_options = mock.Mock()
+        drv.InfobloxPool._get_connector()
+        conn_mock.assert_called_with(expected_opts)
+
+    @mock.patch('networking_infoblox.neutron.ipam.driver.InfobloxPool')
+    @mock.patch('infoblox_client.objects.FixedAddress')
+    def test_allocate_static_ip(self, fa, driver):
+        driver._get_connector = mock.Mock(return_value='connector')
+        subnet_req = requests.SpecificSubnetRequest(
+            'tenant_id', 'subnet_id', '192.168.10.0/24')
+        ipam_sub = drv.InfobloxSubnet(subnet_req, mock.Mock())
+        expected_ip = '192.168.1.15'
+        fixed_address_mock = mock.Mock()
+        fixed_address_mock.ip = expected_ip
+        fa.create = mock.Mock(return_value=fixed_address_mock)
+
+        address_req = requests.SpecificAddressRequest(expected_ip)
+        allocated_ip = ipam_sub.allocate(address_req)
+
+        fa.create.assert_called_with('connector',
+                                     network_view='default',
+                                     ip=expected_ip,
+                                     mac='00:00:00:00:00:00')
+        self.assertEqual(expected_ip, allocated_ip)
+
+    @mock.patch('networking_infoblox.neutron.ipam.driver.InfobloxPool')
+    @mock.patch('infoblox_client.objects.FixedAddress')
+    def test_deallocate_ip(self, fa, driver):
+        driver._get_connector = mock.Mock(return_value='connector')
+        subnet_req = requests.SpecificSubnetRequest(
+            'tenant_id', 'subnet_id', '192.168.10.0/24')
+        ipam_sub = drv.InfobloxSubnet(subnet_req, mock.Mock())
+        expected_ip = '192.168.1.15'
+        fixed_address_mock = mock.Mock()
+        fa.search = mock.Mock(return_value=fixed_address_mock)
+
+        ipam_sub.deallocate(expected_ip)
+
+        fa.search.assert_called_with('connector',
+                                     network_view='default',
+                                     ip=expected_ip)
+        fixed_address_mock.delete.assert_called_with()
