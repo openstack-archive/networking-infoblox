64ba12c749a1350de2b4eb94852d901e8491571b
Yue Ko <yko@infoblox.com>
2015-10-27 05:30:59 -0400

    Added iniset of Infoblox IPAM driver configurations for Devstack. Updated infoblox-client version in requirements.txt
    Change-Id: I3d30b05cd19cccf26123e8f4a1ad07b7e62614df

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -15,7 +15,34 @@ function run_db_migration_for_networking_infoblox {
 
 function configure_networking_infoblox {
     echo_summary "Running db migration for Infoblox Networking"
+
     run_db_migration_for_networking_infoblox
+
+    # Main Configurations
+    iniset $NEUTRON_CONF DEFAULT ipam_driver ""
+    iniset $NEUTRON_CONF infoblox network_view_scope $NETWORKING_INFOBLOX_NETWORK_VIEW_SCOPE
+    iniset $NEUTRON_CONF infoblox default_network_view $NETWORKING_INFOBLOX_DEFAULT_NETWORKVIEW
+    iniset $NEUTRON_CONF infoblox default_host_name_pattern $NETWORKING_INFOBLOX_DEFAULT_HOST_NAME_PATTERN
+    iniset $NEUTRON_CONF infoblox default_domain_name_pattern $NETWORKING_INFOBLOX_DEFAULT_DOMAIN_NAME_PATTERN
+    iniset $NEUTRON_CONF infoblox default_ns_group $NETWORKING_INFOBLOX_DEFAULT_NS_GROUP
+    iniset $NEUTRON_CONF infoblox cloud_data_center_id $NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID
+    iniset $NEUTRON_CONF infoblox allow_admin_network_deletion $NETWORKING_INFOBLOX_ALLOW_ADMIN_NETWORK_DELETION
+    iniset $NEUTRON_CONF infoblox use_host_records_for_ip_allocation $NETWORKING_INFOBLOX_USE_RECORDS_FOR_IP_ALLOCATION
+    iniset $NEUTRON_CONF infoblox bind_dns_records_to_fixed_address $NETWORKING_INFOBLOX_BIND_DNS_RECORD_TYPES
+    iniset $NEUTRON_CONF infoblox unbind_dns_records_from_fixed_address $NETWORKING_INFOBLOX_UNBIND_DNS_RECORD_TYPES
+    iniset $NEUTRON_CONF infoblox delete_dns_records_associated_with_fixed_address $NETWORKING_INFOBLOX_DELETE_DNS_RECORD_TYPES
+
+    # Cloud Data Center Configurations
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID grid_master_host $NETWORKING_INFOBLOX_DC_GRID_MASTER_HOST
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID admin_user_name $NETWORKING_INFOBLOX_DC_ADMIN_USER_NAME
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID admin_password $NETWORKING_INFOBLOX_DC_ADMIN_PASSWORD
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID cloud_user_name $NETWORKING_INFOBLOX_DC_CLOUD_USER_NAME
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID cloud_user_password $NETWORKING_INFOBLOX_DC_CLOUD_USER_PASSWORD
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID wapi_version $NETWORKING_INFOBLOX_DC_WAPI_VERSION
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID ssl_verify $NETWORKING_INFOBLOX_DC_SSL_VERIFY
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID http_pool_connections $NETWORKING_INFOBLOX_DC_HTTP_POOL_CONNECTIONS
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID http_pool_maxsize $NETWORKING_INFOBLOX_DC_HTTP_POOL_MAXSIZE
+    iniset $NEUTRON_CONF infoblox-dc:$NETWORKING_INFOBLOX_CLOUD_DATA_CENTER_ID http_request_timeout $NETWORKING_INFOBLOX_DC_HTTP_REQUEST_TIMEOUT
 }
 
 DIR_INFOBLOX=$DEST/networking-infoblox
