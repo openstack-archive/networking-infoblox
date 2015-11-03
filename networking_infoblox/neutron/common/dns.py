5af0a8b7e3cd39e49f9512b2d72a72162ef61010
Hosung Hwang <hhwang@infoblox.com>
2015-11-03 09:03:16 -0800

    DNS Controller
    DNS controller creates/deletes dns zones and binds/unbinds FQDN to
    and from an ip address to create/delete DNS records.
    
    Domain suffix name is used to generate a FQDN and it can be configured
    to use either a static name or a dynamic name using various patterns.
    A static name is like "infoblox.com" literally.
    A dynamic name is a pattern that can be replaced with a real value.
    The allowed dynamic patterns are {subnet_id}, {subnet_name}, {network_id},
    {network_name}, {tenant_id}, {tenant_name}, {address_scope_id} and
    {address_scope_name}.
    
    When a neutron object name is used rather than an object id such as
    {subnet_name}, {network_name}, and so on, it is the user's responsibilty
    that neutron object names have to be unique.
    
    When deleting a dns zone, the controller checks whether the zone can be
    removed or not by looking at any dependency. For example, deleting a subnet
    action can trigger deleting a zone. In this case, we check what pattern is
    used in the zone name. Let's say that a network id or name is used.
    Then the zone will not be removed if more than one subnets exist. The zone can
    be removed if only one subnet remains so that no other subnet is sharing
    the zone name.
    
    Change-Id: I1f8df0cbc480e6832a380da52f07a03f445ac7c6

--- networking_infoblox/neutron/common/dns.py
+++ networking_infoblox/neutron/common/dns.py
@@ -14,6 +14,15 @@
 #    under the License.
 
 from oslo_log import log as logging
+from oslo_utils import excutils
+
+from neutron.i18n import _LE
+
+from infoblox_client import exceptions as ibc_exc
+
+from networking_infoblox.neutron.common import pattern
+from networking_infoblox.neutron.common import utils
+from networking_infoblox.neutron.db import infoblox_db as dbi
 
 
 LOG = logging.getLogger(__name__)
@@ -21,17 +30,152 @@ LOG = logging.getLogger(__name__)
 
 class DnsController(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, ib_context):
+        self.ib_cxt = ib_context
+        self.grid_config = self.ib_cxt.grid_config
+        self.grid_id = self.grid_config.grid_id
+        self.pattern_builder = pattern.PatternBuilder(self.ib_cxt)
+        self.dns_zone = self.pattern_builder.get_zone_name()
 
     def create_dns_zones(self):
-        pass
+        ea_zone = None
 
-    def delete_dns_zones(self, context, subnet):
-        pass
+        cidr = self.ib_cxt.subnet['cidr']
+        subnet_name = self.ib_cxt.subnet['name']
+        dns_view = self.ib_cxt.mapping.dns_view
+        ns_group = self.grid_config.ns_group
+        zone_format = "IPV%s" % self.ib_cxt.subnet['ip_version']
 
-    def bind_names(self, context, backend_port):
-        pass
+        prefix = utils.get_ipv4_network_prefix(cidr, subnet_name)
 
-    def unbind_names(self, context, backend_port):
-        pass
+        ib_zone = None
+        ib_zone_cidr = None
+
+        try:
+            if ns_group:
+                ib_zone = self.ib_cxt.ibom.create_dns_zone(
+                    dns_view,
+                    self.dns_zone,
+                    ns_group=ns_group,
+                    extattrs=ea_zone)
+                ib_zone_cidr = self.ib_cxt.ibom.create_dns_zone(
+                    dns_view,
+                    cidr,
+                    prefix=prefix,
+                    zone_format=zone_format,
+                    extattrs=ea_zone)
+            else:
+                ib_zone = self.ib_cxt.ibom.create_dns_zone(
+                    dns_view,
+                    self.dns_zone,
+                    grid_primary=None,
+                    grid_secondaries=None,
+                    extattrs=ea_zone)
+                ib_zone_cidr = self.ib_cxt.ibom.create_dns_zone(
+                    dns_view,
+                    cidr,
+                    grid_primary=None,
+                    prefix=prefix,
+                    zone_format=zone_format,
+                    extattrs=ea_zone)
+        except Exception as ex:
+            with excutils.save_and_reraise_exception():
+                LOG.exception(_LE("An exception occurred during dns zone "
+                                  "creation: %s"), ex)
+                if ib_zone:
+                    self.ib_cxt.ibom.delete_dns_zone(dns_view, self.dns_zone)
+                if ib_zone_cidr:
+                    self.ib_cxt.ibom.delete_dns_zone(dns_view, cidr)
+
+    def delete_dns_zones(self):
+        dns_view = self.ib_cxt.mapping.dns_view
+        cidr = self.ib_cxt.subnet['cidr']
+
+        # delete forward zone
+        if self._is_zone_removable():
+            self.ib_cxt.ibom.delete_dns_zone(dns_view, self.dns_zone)
+
+        # delete reverse zone
+        self.ib_cxt.ibom.delete_dns_zone(dns_view, cidr)
+
+    def _is_zone_removable(self):
+        session = self.ib_cxt.context.session
+        pattern = self.grid_config.default_domain_name_pattern
+
+        is_netview_shared = self.ib_cxt.mapping.shared
+        is_network_external = self.ib_cxt.network['router:external']
+        is_network_shared = self.ib_cxt.network['shared']
+        subnet_id = self.ib_cxt.subnet['id']
+        network_id = self.ib_cxt.subnet['network_id']
+        tenant_id = self.ib_cxt.subnet['tenant_id']
+
+        zone_removable = ((is_netview_shared is False and
+                           is_network_external is False and
+                           is_network_shared is False) or
+                          self.grid_config.admin_network_deletion)
+        if zone_removable is False:
+            return False
+
+        # check all dynamic patterns from bottom to top hierarchy
+        subnet_used = '{subnet_name}' in pattern or '{subnet_id}' in pattern
+        if subnet_used:
+            return True
+
+        network_used = '{network_name}' in pattern or '{network_id}' in pattern
+        if network_used:
+            if dbi.is_last_subnet_in_network(session, subnet_id, network_id):
+                return True
+
+        tenant_used = '{tenant_name}' in pattern or '{tenant_id}' in pattern
+        if tenant_used:
+            if dbi.is_last_subnet_in_tenant(session, subnet_id, tenant_id):
+                return True
+
+        address_scope_used = ('{address_scope_name}' in pattern or
+                              '{address_scope_id}' in pattern)
+        if address_scope_used:
+            if dbi.is_last_subnet_in_address_scope(session, subnet_id):
+                return True
+
+        # now check for static zone
+        return dbi.is_last_subnet_in_private_networks(session, subnet_id)
+
+    def bind_names(self, ip_address, hostname=None, port_id=None,
+                   port_tenant_id=None, device_id=None, device_owner=None):
+        if not device_owner:
+            return
+
+        # TODO(hhwang): generate EA for ip address
+        # tenant_id = port_tenant_id or self.ib_cxt.context.tenant_id or 'None'
+        ea_ip_address = None
+
+        try:
+            self._bind_names(self.ib_cxt.ip_alloc.bind_names, ip_address,
+                             hostname, port_id, port_tenant_id, device_id,
+                             device_owner, ea_ip_address)
+        except ibc_exc.InfobloxCannotCreateObject:
+            with excutils.save_and_reraise_exception():
+                self.unbind_names(ip_address, hostname, port_id,
+                                  port_tenant_id, device_id, device_owner)
+
+    def unbind_names(self, ip_address, hostname=None, port_id=None,
+                     port_tenant_id=None, device_id=None, device_owner=None):
+        if not device_owner:
+            return
+
+        self._bind_names(self.ib_cxt.ip_alloc.unbind_names, ip_address,
+                         hostname, port_id, port_tenant_id, device_id,
+                         device_owner)
+
+    def _bind_names(self, binding_func, ip_address, hostname=None,
+                    port_id=None, port_tenant_id=None, device_id=None,
+                    device_owner=None, ea_ip_address=None):
+        network_view = self.ib_cxt.mapping.network_view
+        dns_view = self.ib_cxt.mapping.dns_view
+
+        hostname = self.pattern_builder.get_hostname(ip_address, hostname,
+                                                     port_id, device_owner,
+                                                     device_id)
+        fqdn = str.format("{}.{}", hostname, self.dns_zone)
+
+        binding_func(network_view, dns_view, ip_address, fqdn, ea_ip_address)
