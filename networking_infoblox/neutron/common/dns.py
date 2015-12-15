7741285ac6de0af2341f5c03acc12b9be20361cb
Hosung Hwang <hhwang@infoblox.com>
2015-12-14 23:40:29 -0800

    Fix DNS Hostname Pattern for DHCP Port IP
    For DHCP port IP, DNS host record name is not using the pattern correctly
    because binding name skips for DHCP port ip.
    
    Fixed https://jira.inca.infoblox.com/browse/OPENSTACK-744
    
    Change-Id: Id3d9f29050dc49361f6b211abf9138b01374a566

--- networking_infoblox/neutron/common/dns.py
+++ networking_infoblox/neutron/common/dns.py
@@ -146,7 +146,7 @@ class DnsController(object):
 
     def bind_names(self, ip_address, instance_name=None, port_id=None,
                    port_tenant_id=None, device_id=None, device_owner=None):
-        if not device_owner or device_owner == n_const.DEVICE_OWNER_DHCP:
+        if not device_owner:
             return
 
         tenant_id = port_tenant_id or self.ib_cxt.context.tenant_id
