f6c52f485e4b5c0c90ab89d480c2b7cb816dabb0
Hosung Hwang <hhwang@infoblox.com>
2016-03-07 14:54:19 -0800

    Network View Participation
    The network views can be selected individually to participate in OpenStack
    by adding "Cloud Adapter ID" EA to a network view.
    
    However, "Cloud Adapter ID" EA is created for cloud API so only cloud API
    has READ/WRITE permission. This means that the user cannot use GUI to add
    this EA to a network view.
    
    In order to resolve this inconvenience, we extented the create_ea_defs tool
    script to handle network view participation.
    
    Change-Id: Iad13df331dee1a962afb08ac3ce8d08541ce9876

--- networking_infoblox/tools/create_ea_defs.py
+++ networking_infoblox/tools/create_ea_defs.py
@@ -16,6 +16,7 @@
 
 import getpass
 import os
+import six
 import sys
 
 from oslo_config import cfg
@@ -32,6 +33,8 @@ from networking_infoblox.neutron.common import utils
 
 ENV_SUPERUSER_USERNAME = 'NETWORKING_INFOBLOX_SUPERUSER_USERNAME'
 ENV_SUPERUSER_PASSWORD = 'NETWORKING_INFOBLOX_SUPERUSER_PASSWORD'
+
+PRINT_LINE = 80
 
 LOG = logging.getLogger(__name__)
 
@@ -52,6 +55,18 @@ cfg.CONF(args=sys.argv[1:])
 
 
 def main():
+    cfg.CONF(args=sys.argv[1:],
+             default_config_files=['/etc/neutron/neutron.conf'])
+    common_config.setup_logging()
+    config.register_infoblox_ipam_opts(cfg.CONF)
+    grid_id = cfg.CONF.infoblox.cloud_data_center_id
+    config.register_infoblox_grid_opts(cfg.CONF, grid_id)
+
+    create_ea_defs()
+    participate_network_views(grid_id)
+
+
+def get_credentias():
     credentials = None
     # User command line arguments if specified
     if cfg.CONF.username and cfg.CONF.password:
@@ -68,7 +83,6 @@ def main():
                 credentials = {'username': username, 'password': password}
 
     if not credentials and not cfg.CONF.script:
-        print("\n\n")
         print("In order to create Extensible Attribute definitions,")
         print("superuser privilege is required.\n")
         print("If the preconfigured credentials already has superuser ")
@@ -79,15 +93,18 @@ def main():
         if len(username) > 0:
             password = getpass.getpass("Enter password: ")
             credentials = {'username': username, 'password': password}
+        print("\n")
+    return credentials
 
-    cfg.CONF(args=sys.argv[1:],
-             default_config_files=['/etc/neutron/neutron.conf'])
-    common_config.setup_logging()
-    config.register_infoblox_ipam_opts(cfg.CONF)
-    grid_id = cfg.CONF.infoblox.cloud_data_center_id
-    config.register_infoblox_grid_opts(cfg.CONF, grid_id)
 
+def create_ea_defs():
+    print("\nCreating EA definitions...")
+    print("-" * PRINT_LINE)
+    print("")
+
+    credentials = get_credentias()
     conn = utils.get_connector(credentials)
+
     if not (utils.get_features(conn).create_ea_def):
         LOG.error("WAPI Version '%s' is not supported - Script ABORTED!",
                   conn.wapi_version)
@@ -96,8 +113,12 @@ def main():
     mgr = object_manager.InfobloxObjectManager(conn)
     ea_defs_created = mgr.create_required_ea_definitions(
         const.REQUIRED_EA_DEFS)
-    LOG.info("The following EA Definitions have been created: '%s'" %
-             [ea_def['name'] for ea_def in ea_defs_created])
+    if ea_defs_created:
+        print("The following EA Definitions have been created: '%s'" %
+              [ea_def['name'] for ea_def in ea_defs_created])
+    else:
+        print("All the EAs has been already created.")
+    print("\n")
 
     host_ip = getattr(conn, 'host')
     if ib_utils.determine_ip_version(host_ip) == 4:
@@ -110,6 +131,9 @@ def main():
                   host_ip)
         exit(1)
 
+    print("Adding grid configuration EAs to the grid master...")
+    print("-" * PRINT_LINE)
+    print("")
     ea_set = {}
     if member.extattrs is None:
         member.extattrs = objects.EA({})
@@ -120,9 +144,96 @@ def main():
             member.extattrs.set(ea, val)
 
     if ea_set:
-        LOG.info("Adding the following EA values to Grid Member: '%s'" %
-                 ea_set)
+        print("Grid configurations: '%s'" % ea_set)
         member.update()
+    else:
+        print("All the grid configurations have been already added.")
+    print("\n")
+
+
+def participate_network_views(grid_id):
+    print("Participating network views for Openstack...")
+    print("-" * PRINT_LINE)
+    print("")
+
+    conn = utils.get_connector()
+
+    ib_netviews = objects.NetworkView.search_all(conn)
+    if not ib_netviews:
+        print("No network view exists\n")
+        return
+
+    netview_names = [ib_netview.name for ib_netview in ib_netviews]
+    print ("Found %s network views from the grid.\n" % len(netview_names))
+    question = ("Do you wish to select network views and make them "
+                "available for Openstack?")
+    choice = ask_question(question)
+    if choice != 'y':
+        return
+
+    question = "Do you want to list network views?"
+    choice = ask_question(question)
+    if choice == 'y':
+        print (', '.join(netview_names))
+        print("")
+    question = ("Please provide a comma separated list of "
+                "network views: ")
+    netview_input = raw_input(question)
+    if not netview_input:
+        return
+
+    netview_list = []
+    [netview_list.append(x.strip()) for x in netview_input.split(',')
+     if x and x not in netview_list]
+    if not netview_list:
+        return
+
+    for nv in netview_list:
+        ib_netview_found = [ib_net for ib_net in ib_netviews
+                            if ib_net.name == nv]
+        if not ib_netview_found:
+            print("'%s' is not found." % nv)
+            continue
+
+        ib_netview = ib_netview_found[0]
+        ea_netview = eam.get_ea_for_network_view(None, None, grid_id)
+
+        if ib_netview.extattrs is None:
+            ib_netview.extattrs = ea_netview
+        else:
+            cloud_adapter_ids = ib_netview.extattrs.get(
+                const.EA_CLOUD_ADAPTER_ID)
+            if cloud_adapter_ids:
+                if isinstance(cloud_adapter_ids, six.string_types):
+                    cloud_adapter_ids = [cloud_adapter_ids]
+
+                found_ids = [id for id in cloud_adapter_ids
+                             if id == grid_id]
+                if found_ids:
+                    print("'%s' already participated." % nv)
+                    continue
+
+                cloud_adapter_ids.append(grid_id)
+            else:
+                cloud_adapter_ids = [grid_id]
+
+            ib_netview.extattrs.set(const.EA_CLOUD_ADAPTER_ID,
+                                    cloud_adapter_ids)
+        ib_netview.update()
+        print("'%s' is participated." % nv)
+    print("\n")
+
+
+def ask_question(question):
+    while True:
+        choice = raw_input("%s Enter 'y' or 'n': " % question)
+        if choice not in ['y', 'n']:
+            print ("Enter a valid choice. Please try again.\n")
+            continue
+        else:
+            break
+    print ("")
+    return choice
 
 
 if __name__ == "__main__":
